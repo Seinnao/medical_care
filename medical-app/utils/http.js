94ba783c5b515ed99dfdc806a05b347283e3df2e
Administrator <364058721@qq.com>
2023-04-11 01:07:03 +0800

    小程序初步完成

---
+++ medical-app/utils/http.js
@@ -0,0 +1,69 @@
+//let bastUrl="https://www.jcoolish.top/network/api/"
+let bastUrl="http://127.0.0.1/api/"
+
+const request={}
+
+request.get = function(url,data){
+	
+	let token = uni.getStorageSync('token');
+	const header={}
+	
+	if(token){
+		header['Authorization']='Bearer '+token
+	}
+	return new Promise((resolve, reject)=>{
+		uni.request({
+		    url: bastUrl+url, //仅为示例，并非真实接口地址。
+		    data:data,
+		    header:header,
+		    success: (res) => {
+				if(res.statusCode === 401){
+					uni.showToast({
+						title: '登录超时',
+						icon: 'error'
+					});	
+				}else if(res.statusCode === 200){
+					resolve(res.data)
+				}
+		    },
+			fail: (err) =>{
+				reject(err)
+			}
+		});
+	})
+}
+
+request.post = function(url,data){
+	
+	let token = uni.getStorageSync('token');
+	const header={}
+	header['content-type']='application/json; charset=utf-8'
+	
+	if(token){
+		header['Authorization'] = 'Bearer '+token;
+	}
+	return new Promise((resolve, reject)=>{
+		uni.request({
+		    url: bastUrl+url, //仅为示例，并非真实接口地址。
+		    data:data,
+			method:'POST',
+		    header:header,
+		    success: (res) => {
+				//console.log(res)
+				if(res.statusCode === 401){
+					uni.showToast({
+						title: '登录超时',
+						icon: 'error'
+					});	
+				}else if(res.statusCode === 200){
+					resolve(res.data)
+				}
+		    },
+			fail: (err) =>{
+				reject(err)
+			}
+		});
+	})
+}
+
+export default request 
